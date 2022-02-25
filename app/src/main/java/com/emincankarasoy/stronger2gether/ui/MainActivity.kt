ce12e6b15de143016989ceecaca9296f427d8780
Emincan Karasoy <emincannkarasoy@gmail.com>
2022-02-25 22:14:09 +0300

    Add splash screen

--- app/src/main/java/com/emincankarasoy/stronger2gether/ui/MainActivity.kt
+++ app/src/main/java/com/emincankarasoy/stronger2gether/ui/MainActivity.kt
@@ -1,16 +1,28 @@
-package com.emincankarasoy.stronger2gether.ui.view
+package com.emincankarasoy.stronger2gether.ui
 
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.emincankarasoy.stronger2gether.R
 import com.emincankarasoy.stronger2gether.module.checkExitRequest
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Handler().postDelayed(Runnable{goToNextScreen()},5000L)
+    }
+
+    private fun goToNextScreen(){
+
     }
 
     override fun onBackPressed() {
